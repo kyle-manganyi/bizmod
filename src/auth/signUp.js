7f2c047f2d9c5dffc7d0d8a80ac3b86a510e6c24
kyle <manganyikyle@gmail.com>
2020-08-09 19:08:15 +0200

    init

--- src/auth/signUp.js
+++ src/auth/signUp.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './App.css';
+import '../App.css';
 import { Button } from 'semantic-ui-react'
 
 function App() {
--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import SignIn from './auth/signIn'
+import SignUp from './auth/signUp'
 import * as serviceWorker from './serviceWorker';
+import 'semantic-ui-css/semantic.min.css'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const routing = (
+  <Router>
+    <Switch>
+      <Route path="/" component={App} exact/>
+      <Route path="/signin" component={SignIn} exact/>
+      <Route path="/signup" component={SignIn} exact/>
+    </Switch>
+  </Router>
+)
 
+ReactDOM.render(routing, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
