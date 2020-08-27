78e9e9e5d469337a96410c2336ede2947454f5ec
MJ Macheli <machelimail@gmail.com>
2020-08-27 20:20:31 +0200

    cvs

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,5 +1,7 @@
 import React,{ useState,Fragment} from 'react'
-import { Card, } from 'semantic-ui-react'
+import { Card,Segment,Header,Icon,Button } from 'semantic-ui-react'
+
+import './Dashboard.css'
 
 const Dashboard = () => {
 
@@ -8,22 +10,26 @@ const Dashboard = () => {
     const fmtt = (data) => {
 
         const mydata = []
-        for (const [_, values] of Object.entries(data)) {
+        for (const [ _,values ] of Object.entries(data)) {
             const component = []
             for (const [key, value] of Object.entries(values)) {
-                component.push(key + '\t\t\t'+ value)
+                component.push({key, value})
             }
             mydata.push(component)
         }
-        console.log(mydata)
         setCv(mydata)
     }
 
-    const testing = (x) => x.map(i =><div>{i}</div>)
+    const testing = (x) => x.map(i =><div key={i.key}><span className="keys">{i.key}</span><span className="values">{i.value}</span></div>)
 
     return (
         <Fragment>
-        <form>
+        <Segment placeholder>
+                <Header icon>
+                <Icon name='file word outline' />
+                Select files to upload
+            </Header>
+            <Button secondary size='huge'>
             <input type='file' name='cv' multiple onChange={e => {
                 var formdata = new FormData();
                 for(let i = 0; i < e.target.files.length; i++){
@@ -39,10 +45,11 @@ const Dashboard = () => {
                 .then(result => fmtt( JSON.parse(result) ))
                 .catch(error => console.log('error', error));
             }}/>
-        </form>
+            </Button>
+        </Segment>
         {
             cv.length > 0 ? cv.map(x =>(
-               <Card>
+               <Card key={x} className="cvs">
                 {testing(x)}
                </Card>
             ))
